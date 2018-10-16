0f2209be018f338ac0d9749bbc75d40fec299be8
Tymoshok Dmytro <dtymochok@auchan.ua>
2018-10-16 17:47:05 +0300

    Add the less03 folder

---
+++ src/less03/ImplementedClass.java
@@ -0,0 +1,11 @@
+package less03;
+
+public class ImplementedClass implements MyInt{
+
+	@Override
+	public void someMethod() {
+		System.out.println("Implemented method in class");
+		
+	}
+
+}
---
+++ src/less03/NIOExample.java
@@ -0,0 +1,31 @@
+package less03;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+public class NIOExample {
+
+	public static void main(String[] args) {
+		Path path=Paths.get(
+"C:\\Users\\Java\\eclipse-workspace\\AdvO\\src\\less03\\MyClient.java");
+		System.out.println(Files.isExecutable(path));
+		System.out.println(Files.isReadable(path));
+		File file=new File(path.toUri());
+		file.mkdir();
+		Path toCopy=Paths.get(
+"C:\\Users\\Java\\eclipse-workspace\\AdvO\\src\\MyClient_copy.java");
+		
+		try {
+			//Files.copy(path, toCopy);
+			Files.move(path, toCopy, StandardCopyOption.REPLACE_EXISTING);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
